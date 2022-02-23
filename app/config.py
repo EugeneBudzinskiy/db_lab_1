6b06bd9d7deee1f9885f0e8217a0172a7ece19d4
eugene.budzinskiy@gmail.com <75k8f2gtma>
2022-02-23 18:00:53 +0200

    Initial upload

---
+++ app/config.py
@@ -0,0 +1,9 @@
+TABLE_NAME = 'zno_data'
+
+TIME_RECORD_PATH = 'time.txt'
+
+RESULT_PLOT_PATH = 'result.png'
+
+RESULT_CSV_PATH = 'result.csv'
+RESULT_CSV_ENCODING = 'utf-8'
+RESULT_CSV_DELIMITER = ';'
